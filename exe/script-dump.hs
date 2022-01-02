3ea6ee4c5f2b209bd6895b4add27d1f384bbfeae
Riley-Kilgore <rileyk9@icloud.com>
2022-01-02 08:51:24 -0700

    Finalized Escrow.. Hopefully no bugs?

---
+++ exe/script-dump.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings   #-}
+import           Prelude
+import           System.Environment
+
+import           Cardano.Api
+import           Cardano.Api.Shelley
+import           Codec.Serialise
+
+import qualified Cardano.Ledger.Alonzo.Data as Alonzo
+import qualified Plutus.V1.Ledger.Api as Plutus
+
+import qualified Data.ByteString.Short as SBS
+import qualified Data.ByteString.Lazy as LB
+
+import           Ledger                     (datumHash, scriptHashAddress)
+
+import           Escrow
+
+import           Plutus.V1.Ledger.Value
+import           Plutus.V1.Ledger.Api
+import           Data.String                         (IsString (..))
+import           Data.Aeson
+import           GHC.Num (encodeDoubleInteger)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let nargs = length args
+  if nargs /= 0 then
+    do
+      putStrLn $ "Usage:"
+      putStrLn $ "script-dump"
+  else 
+    do 
+      let
+        scriptnum = 42
+        validatorname = "validator.plutus"
+
+        validatorAsCbor = serialise tradeValidator
+        validatorShortBs = SBS.toShort . LB.toStrict $ validatorAsCbor
+        validatorScript = PlutusScriptSerialised validatorShortBs
+
+      putStrLn $ "Writing output to: " ++ validatorname
+      writePlutusScript' scriptnum validatorname validatorScript validatorShortBs
+
+      writeFile "validator-hash.txt" (show $ tradeValidatorHash)
+
+writePlutusScript' :: Integer -> FilePath -> PlutusScript PlutusScriptV1 -> SBS.ShortByteString -> IO ()
+writePlutusScript' scriptnum filename scriptSerial scriptSBS =
+  do
+  case Plutus.defaultCostModelParams of
+        Just m ->
+          let Alonzo.Data pData = toAlonzoData (ScriptDataNumber scriptnum)
+              (logout, e) = Plutus.evaluateScriptCounting Plutus.Verbose m scriptSBS [pData]
+          in do print ("Log output" :: String) >> print logout
+                case e of
+                  Left evalErr -> print ("Eval Error" :: String) >> print evalErr
+                  Right exbudget -> print ("Ex Budget" :: String) >> print exbudget
+        Nothing -> error "defaultCostModelParams failed"
+  result <- writeFileTextEnvelope filename Nothing scriptSerial
+  case result of
+    Left err -> print $ displayError err
+    Right () -> return ()
